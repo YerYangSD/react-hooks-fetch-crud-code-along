b16ab7e7a60ed75f9164369d3163ac4830d79676
Yer Yang <yyhmg_86@yahoo.com>
2023-09-27 23:22:53 -0700

    updates items

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -13,6 +13,17 @@ function ShoppingList() {
       .then(data => setItems(data))
   }, [])
 
+  function handleUpdatedItem(updatedItem) {
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id) {
+        return updatedItem
+      } else {
+        return item
+      }
+    })
+    setItems(updatedItems)
+  }
+
   function handleAddItem(newItem) {
     setItems([...items, newItem])
   }
@@ -36,7 +47,7 @@ function ShoppingList() {
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdatedItem={handleUpdatedItem} />
         ))}
       </ul>
     </div>
